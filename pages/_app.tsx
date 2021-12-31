c0bc4d6fa35ae902bdcb693709dd0a6fe529971c
Amol Borkar <amolborkar5@gmail.com>
2021-12-31 14:00:11 +0530

    Initial Implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Layout from './layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Layout><Component {...pageProps} /></Layout>
 }
 
 export default MyApp
