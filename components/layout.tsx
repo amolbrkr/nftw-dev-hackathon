90995b4cec0f6e38043ad188c8abcb15dfb04204
Amol Borkar <amolborkar5@gmail.com>
2022-01-01 19:15:55 +0530

    Style changes for footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,8 +9,8 @@ const Layout: NextPage<any> = ({ children }) => {
         {children}
       </main>
       <footer className='has-background-black'>
-        <p>Built with love for MongoDB Atlas Hackathon on <a href="https://dev.to">DEV</a> by <strong className='has-text-white'>Amol Borkar</strong> (View full code here).</p>
-        <p>Powered by superb <a href="https://mongodb.com">MongoDB</a> products like <a href="https://www.mongodb.com/atlas/search">Atlas Search</a>, <a href="https://www.mongodb.com/realm/appdev">Realm Functions</a> and <a href="https://www.mongodb.com/atlas/database">Database.</a></p>
+        <p>Built with love for MongoDB Atlas Hackathon on <a href="https://dev.to">DEV</a> by <strong className='has-text-white'>Amol Borkar</strong>.</p>
+        <p className='p1'>Powered by superb <a href="https://mongodb.com">MongoDB</a> products like <a href="https://www.mongodb.com/atlas/search">Atlas Search</a>, <a href="https://www.mongodb.com/realm/appdev">Realm Functions</a> and <a href="https://www.mongodb.com/atlas/database">Database.</a> All the assets listed on this website are fetched from the <a href="https://docs.opensea.io/reference/api-overview">OpenSea NFT API.</a> All rights go to their respective owners.</p>
       </footer>
     </>
   )
