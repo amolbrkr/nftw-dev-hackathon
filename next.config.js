59d54d7524adf6388ce6e69ae2e2157624d397a0
Amol Borkar <amolborkar5@gmail.com>
2022-01-01 19:14:33 +0530

    Added redirect for homepage

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com', 'storage.googleapis.com', 'storage.opensea.io']
-  }
-}
+    domains: [
+      "lh3.googleusercontent.com",
+      "storage.googleapis.com",
+      "storage.opensea.io",
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/assets",
+        permanent: true,
+      },
+    ];
+  },
+};
