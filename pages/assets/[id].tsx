6cb958d14855faa342258f17db09ab73a7ac44d3
Amol Borkar <amolborkar5@gmail.com>
2022-01-02 19:31:28 +0530

    Moved api to internal

--- pages/assets/[id].tsx
+++ pages/assets/[id].tsx
@@ -9,14 +9,14 @@ const Asset = () => {
   useEffect(() => {
     if (router.asPath !== router.route) {
       const { id } = router.query
-      const [contractAddr, tokenId] = id.split('__')
-      fetch(`https://api.opensea.io/api/v1/asset/${contractAddr}/${tokenId}/`).then(res => {
-        res.json().then(data => { setAsset(data); console.log(data); });
-      })
+      fetch(`http://localhost:3000/api/assets/${id}`).then(res => res.json()).then(data => setAsset(data));
     }
   }, [router]);
 
-  return asset ? <AssetDetail asset={asset} /> : <div className='is-flex is-justify-content-center is-align-items-center'><h3 className='title is-5'>Loading </h3><span className="loader-mixin"></span></div>;
+  return asset ? <AssetDetail asset={asset} /> : (
+    <div className='is-flex is-justify-content-center is-align-items-center'>
+      <h3 className='title is-5'>Loading </h3><br /><span className="loader-mixin"></span>
+    </div>);
 }
 
 export default Asset;
