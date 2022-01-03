b762f5b2376d44d066828dafbe7dcd6f4ddc5a8f
Amol Borkar <amolborkar5@gmail.com>
2022-01-04 00:37:19 +0530

    process test

--- lib/util.ts
+++ lib/util.ts
@@ -1,6 +1,7 @@
 export default class Utils {
 
   static get baseUrl() {
+    console.log(process.env.NODE_ENV);
     const inDev = process.env.NODE_ENV !== 'production';
     const dev = process.env.NEXT_PUBLIC_DEV_URL;
     const prod = process.env.NEXT_PUBLIC_DEV_URL;
